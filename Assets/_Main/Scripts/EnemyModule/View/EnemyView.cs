a590abf7bc9fe9875a9a4d69e6583fae2a4f7c2c
Vladislav <GimliVlad@list.ru>
2026-03-20 17:50:23 +0300

    Refactor

--- Assets/_Main/Scripts/EnemyModule/View/EnemyView.cs
+++ Assets/_Main/Scripts/EnemyModule/View/EnemyView.cs
@@ -6,7 +6,6 @@ using Zenject;
 
 namespace EnemyModule
 {
-    //TODO: Need refactor
     public class EnemyView : MonoBehaviour
     {
         private const float DestroyDelay = 10f;
@@ -22,53 +21,36 @@ namespace EnemyModule
         private Vector3? _lastHitPosition;
         private Vector3? _lastHitForce;
 
-        private bool _isHealthSubscribed;
-        private bool _isAttackSubscribed;
-
         [Inject]
         public void Construct(IHealthComponent health, IPathMoveComponent movement, IRangedAttackComponent attack)
         {
-            UnsubscribeFromHealth();
-            UnsubscribeFromAttack();
-
             _health = health;
             _movement = movement;
             _attack = attack;
-
-            SubscribeToHealth();
-            SubscribeToAttack();
         }
 
         private void OnEnable()
         {
-            SubscribeToHealth();
-            SubscribeToAttack();
+            _health.DamageTaken += OnDamageTaken;
+            _health.Died += Die;
+
+            _attack.Attacked += OnAttacked;
         }
 
         private void Update() => UpdateAnimator();
 
         private void OnDisable()
         {
-            UnsubscribeFromHealth();
-            UnsubscribeFromAttack();
+            _health.DamageTaken -= OnDamageTaken;
+            _health.Died -= Die;
+
+            _attack.Attacked -= OnAttacked;
         }
 
-        private void UpdateAnimator()
-        {
-            if (animator == null || _movement == null)
-                return;
-
-            animator.SetBool(EnemyAnimatorHashes.IsMoving, _movement.IsMoving);
-        }
+        private void UpdateAnimator() => animator.SetBool(EnemyAnimatorHashes.IsMoving, _movement.IsMoving);
 
         private void Die()
         {
-            if (ragdoll == null)
-            {
-                Destroy(gameObject);
-                return;
-            }
-
             ragdoll.Activate(_lastHitForce, _lastHitPosition);
             Destroy(gameObject, DestroyDelay);
         }
@@ -79,50 +61,6 @@ namespace EnemyModule
             _lastHitForce = hitForce;
         }
 
-        private void OnAttacked()
-        {
-            if (animator == null)
-                return;
-
-            animator.SetTrigger(EnemyAnimatorHashes.Shoot);
-        }
-
-        private void SubscribeToHealth()
-        {
-            if (_health == null || _isHealthSubscribed)
-                return;
-
-            _health.DamageTaken += OnDamageTaken;
-            _health.Died += Die;
-            _isHealthSubscribed = true;
-        }
-
-        private void UnsubscribeFromHealth()
-        {
-            if (_health == null || !_isHealthSubscribed)
-                return;
-
-            _health.DamageTaken -= OnDamageTaken;
-            _health.Died -= Die;
-            _isHealthSubscribed = false;
-        }
-
-        private void SubscribeToAttack()
-        {
-            if (_attack == null || _isAttackSubscribed)
-                return;
-
-            _attack.Attacked += OnAttacked;
-            _isAttackSubscribed = true;
-        }
-
-        private void UnsubscribeFromAttack()
-        {
-            if (_attack == null || !_isAttackSubscribed)
-                return;
-
-            _attack.Attacked -= OnAttacked;
-            _isAttackSubscribed = false;
-        }
+        private void OnAttacked() => animator.SetTrigger(EnemyAnimatorHashes.Shoot);
     }
 }
