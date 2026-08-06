a8ea0c649aad19b79ba93d82407aa653a3cda22e
Ivan <killermira2013@gmail.com>
2026-08-06 04:21:10 +0300

    7 8 11

--- Assets/_Main/Scripts/BuildingModule/Runtime/View/BuildingView.cs
+++ Assets/_Main/Scripts/BuildingModule/Runtime/View/BuildingView.cs
@@ -1,14 +1,31 @@
 using UnityEngine;
 using InventoryModule;
+using BaseModule;
 
 namespace BuildingModule
 {
-    public class BuildingView : MonoBehaviour
+    public class BuildingView : MonoBehaviour, IHasInstanceId
     {
         [SerializeField] private Collider collisionCollider;
 
+        private string _buildingConfigId;
+
+        private string _instanceId;
+
         public Collider CollisionCollider => collisionCollider;
         public IExternalUI ExternalUI => GetComponent<IExternalUI>();
+
+        public string BuildingConfigId
+        {
+            get => _buildingConfigId;
+            set => _buildingConfigId = value;
+        }
+
+        public string InstanceId
+        {
+            get => _instanceId;
+            set => _instanceId = value;
+        }
 
         public MeshRenderer[] GetAllMeshRenderers()
         {
