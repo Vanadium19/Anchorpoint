cea9ac93c3797905b66d8a60a3fb0d11286f69fc
Denis Belavin <denis.belavin@saritasa.com>
2026-09-05 14:11:18 +0700

    Add weighted random event picker

---
+++ Assets/_Main/Scripts/RandomEventsModule/Runtime/Picking/WeightedRandomPickerAsset.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace RandomEventsModule
+{
+    /// <summary>Asset wrapper for <see cref="WeightedRandomPicker"/>.</summary>
+    [Serializable]
+    public class WeightedRandomPickerAsset : ZenjectRandomEventPickerAsset<WeightedRandomPicker>
+    {
+    }
+}
