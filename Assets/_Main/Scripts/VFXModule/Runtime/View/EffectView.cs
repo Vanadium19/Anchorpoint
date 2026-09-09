09bb7ffc55ec16f082089b763177a062d8e53713
Denis Belavin <denis.belavin@saritasa.com>
2026-09-09 18:42:57 +0700

    Support long-running effects, scene audio DI and held interact input

--- Assets/_Main/Scripts/VFXModule/Runtime/View/EffectView.cs
+++ Assets/_Main/Scripts/VFXModule/Runtime/View/EffectView.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 namespace VFXModule
 {
-    public class EffectView : MonoBehaviour
+    public class EffectView : MonoBehaviour, IEffectHandle
     {
         [SerializeField] private ParticleSystem particleSystem;
 
         private EffectId _id;
+        private int _playVersion;
+        private bool _isPlaying;
 
         public event Action<EffectView> Finished;
 
@@ -18,23 +20,67 @@ namespace VFXModule
 
         public void Initialize(EffectId id) => _id = id;
 
-        public void Play()
+        public void SetScale(Vector3 effectScale)
         {
             if (particleSystem == null)
+                return;
+
+            particleSystem.transform.localScale = effectScale;
+        }
+
+        public void Play()
+        {
+            _playVersion++;
+            _isPlaying = true;
+
+            if (particleSystem == null)
             {
-                Finished?.Invoke(this);
+                Finish();
                 return;
             }
 
             particleSystem.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
             particleSystem.Play(true);
-            FinishAsync().Forget();
+            FinishAsync(_playVersion).Forget();
         }
 
-        private async UniTaskVoid FinishAsync()
+        public void Stop()
+        {
+            if (!_isPlaying)
+                return;
+
+            if (particleSystem == null)
+            {
+                Finish();
+                return;
+            }
+
+            // Stop spawning but let live particles fade out; FinishAsync returns the effect to the pool once none remain.
+            particleSystem.Stop(true, ParticleSystemStopBehavior.StopEmitting);
+        }
+
+        private async UniTaskVoid FinishAsync(int playVersion)
         {
             await UniTask.Yield();
-            await UniTask.WaitWhile(() => particleSystem != null && particleSystem.IsAlive(true));
+
+            await UniTask.WaitWhile(() =>
+                this != null &&
+                playVersion == _playVersion &&
+                particleSystem != null &&
+                particleSystem.IsAlive(true));
+
+            if (this == null || playVersion != _playVersion || !_isPlaying)
+                return;
+
+            Finish();
+        }
+
+        private void Finish()
+        {
+            if (!_isPlaying)
+                return;
+
+            _isPlaying = false;
             Finished?.Invoke(this);
         }
     }
